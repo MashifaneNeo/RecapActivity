b749cd8243ffd17e5802600436e96e3dda816f92
emeris <emeris@LabVM2302769>
2026-07-31 21:02:05 +0200

    created generic RankedList<T> constrained to IComparable<T>

---
+++ Models/RankedList.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace HolidayShowdown.Models;
+public class RankedList<T> where T : IComparable<T>
+{
+    private readonly List<T> _items = new();
+
+    public IReadOnlyList<T> Items => _items;
+
+    public void Add(T item) => _items.Add(item);
+
+    public void Clear() => _items.Clear();
+
+    public IReadOnlyList<T> Ranked => _items.OrderByDescending(i => i).ToList();
+
+    public int Count => _items.Count;
+}
