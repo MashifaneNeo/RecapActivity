fe47753e8aec590bd22aa5b1c2530fc9d0b8182a
emeris <emeris@LabVM2302769>
2026-07-31 23:04:14 +0200

    added ResultView showing verdict and combined total via overloaded operators and HistoryView with ranked leaderboard and favourites personal feature

---
+++ Views/HistoryView.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Linq;
+using System.Windows.Controls;
+using HolidayShowdown.Models;
+
+namespace HolidayShowdown.Views;
+
+public partial class HistoryView : UserControl
+{
+    public HistoryView(RankedList<HolidayCount> history, RankedList<HolidayCount> favourites)
+    {
+        InitializeComponent();
+
+        var ranked = history.Ranked
+            .Select((h, i) => new { Rank = i + 1, h.CountryName, h.Year, h.Count })
+            .ToList();
+
+        HistoryListView.ItemsSource = ranked;
+        FavouritesListView.ItemsSource = favourites.Ranked;
+    }
+}
---
+++ Views/ResultView.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using HolidayShowdown.Models;
+
+namespace HolidayShowdown.Views;
+
+public partial class ResultView : UserControl
+{
+    private readonly HolidayCount _left;
+    private readonly HolidayCount _right;
+    private HolidayCount _winner;
+
+    public event Action<string>? SavedToHistory;
+    public event Action<HolidayCount>? FavouriteAdded;
+    public event EventHandler? BackToCompareRequested;
+
+    public ResultView(HolidayCount left, HolidayCount right)
+    {
+        InitializeComponent();
+        _left = left;
+        _right = right;
+
+        LeftText.Text = _left.ToString();
+        RightText.Text = _right.ToString();
+
+        if (_left > _right)
+        {
+            _winner = _left;
+            VerdictText.Text = $"{_left.CountryName} wins!";
+            VerdictText.Foreground = (System.Windows.Media.Brush)Application.Current.Resources["WinColor"];
+        }
+        else if (_right > _left)
+        {
+            _winner = _right;
+            VerdictText.Text = $"{_right.CountryName} wins!";
+            VerdictText.Foreground = (System.Windows.Media.Brush)Application.Current.Resources["WinColor"];
+        }
+        else if (_left == _right)
+        {
+            _winner = _left;
+            VerdictText.Text = "It's a tie!";
+            VerdictText.Foreground = (System.Windows.Media.Brush)Application.Current.Resources["TieColor"];
+        }
+
+        var combined = _left + _right; 
+        CombinedText.Text = $"Combined total: {combined.Count} public holidays between both countries.";
+
+        SavedToHistory?.Invoke(_winner.CountryName);
+    }
+
+    private void Favourite_Click(object sender, RoutedEventArgs e) => FavouriteAdded?.Invoke(_winner);
+
+    private void Back_Click(object sender, RoutedEventArgs e) => BackToCompareRequested?.Invoke(this, EventArgs.Empty);
+}
